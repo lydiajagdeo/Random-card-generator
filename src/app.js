33377b91b2e0ca8a9547de367d09e9556d98fc18
lydiajagdeo <lydiajagdeo@gmail.com>
2022-06-03 20:57:06 +0000

    Final Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardDiv = document.querySelector(".card");
+  let numberSpan = document.querySelector(".number");
+  let rank = ["A", "J", "Q", "K", 2, 3, 4, 5, 6, 7, 8, 9];
+  let suit = ["spades", "hearts", "diamonds", "clubs"];
+  let selectedSuit = suit[getRandomInt(suit)];
+  console.log(selectedSuit);
+  cardDiv.classList.add(selectedSuit);
+  numberSpan.innerHTML = rank[getRandomInt(rank)];
 };
+function getRandomInt(array) {
+  return Math.floor(Math.random() * array.length);
+}
